2df498d2597e7ad8c199d3cc1d12ec494d3a327c
Rohit  Venkatesan <rohitvenkatesan@Rohits-MacBook-Pro.local>
2024-08-24 14:00:23 +0530

    Added new module for scaling data; train test split; Regression model building.

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 import yaml
 import pandas as pd
 from utils import pandas_transformations as pt
+from utils import basic_ml as ml
 
 with open("project.yaml") as instructions:
     try:
@@ -14,6 +15,7 @@ for instruction in instructionsList:
     current_instruction = instruction.keys()
     if "data_loader" in current_instruction:
         processedData = pt.read_data(instruction)  
+        print(processedData.columns)
     elif "sanitize_data" in current_instruction:
         processedData = pt.sanitize_headers(processedData) 
     elif "filter_data" in current_instruction:
@@ -22,8 +24,14 @@ for instruction in instructionsList:
         processedData = pt.group_data(processedData, instruction)
     elif "single_column_transformer" in current_instruction:
         processedData = pt.feature_engineer_single_col(processedData, instruction)
+    elif "scale_data" in current_instruction:
+        processedData = ml.scale_data(processedData, instruction)
+    elif "split_data" in current_instruction:
+        modelTrainDict = ml.build_train_test_split(processedData, instruction)
+    elif "build_regression_model" in current_instruction:
+        result = ml.build_regression(modelTrainDict, instruction)
     elif "data_store" in current_instruction:
         pt.save_data(processedData, instruction)
         print("Data Saved")
 
-print(processedData)
+print(processedData.head(10))
